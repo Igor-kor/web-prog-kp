f3302ecde6c67df0a04bd13b11c630cab3eac4e0
Igor Sharangiya <a.sharangia@harddays.dev>
2021-09-26 17:03:42 +0300

    Добавил марки

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('marks')" :active="request()->routeIs('marks')">
+                        {{ __('Marks') }}
+                    </x-nav-link>
                 </div>
             </div>
 
