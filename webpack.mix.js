78b35bea65e84706744d045feb4e8f743aabd2d3
Igor Sharangiya <igor_korenovsk@mail.ru>
2021-11-06 21:26:52 +0300

    Подключил tailwindcss #23

--- webpack.mix.js
+++ webpack.mix.js
@@ -28,4 +28,7 @@ mix.browserSync({
 });
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+    ]);
