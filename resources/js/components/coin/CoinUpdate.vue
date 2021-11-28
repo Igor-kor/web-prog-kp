4957cd4ab820aa7c9708dd0ac03afdda8b4b9849
Igor Sharangiya <igor_korenovsk@mail.ru>
2021-11-28 16:37:37 +0300

    refactor

--- resources/js/components/coin/CoinUpdate.vue
+++ resources/js/components/coin/CoinUpdate.vue
@@ -51,7 +51,7 @@ export default {
 }
 </script>
 
-<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style src="../../../../node_modules/vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style scoped>
 
