554c81ec7c5a18a083b393969bc017dee5013913
Igor Sharangiya <igor_korenovsk@mail.ru>
2021-11-28 13:06:00 +0300

    Сделал миграцию для марки #26

--- database/migrations/2021_10_30_154458_create_marks_table.php
+++ database/migrations/2021_10_30_154458_create_marks_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Country;
 
 class CreateMarksTable extends Migration
 {
@@ -16,6 +17,13 @@ class CreateMarksTable extends Migration
         Schema::create('marks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignIdFor(Country::class)->nullable(); // страна
+            $table->string('year')->nullable();// год
+            $table->string('size')->nullable();// размер
+            $table->string('denomination')->nullable(); // номинал
+            $table->string('material')->nullable(); // материал
+            $table->string('circulation')->nullable(); // тираж
+            $table->string('features')->nullable(); // Особенности
         });
     }
 
